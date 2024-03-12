1fc576e8ca2f506e6679f937a612b0a7f78f3cee
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-03-12 13:50:58 +0000

    add routing to app

--- filrougeFront/src/App.jsx
+++ filrougeFront/src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes ,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 
@@ -15,7 +12,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-
       </BrowserRouter>
     </>
   )
