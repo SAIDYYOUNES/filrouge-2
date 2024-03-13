49253e1ca88865e71289a3e8e6ee4fe22134d209
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-03-13 14:39:57 +0000

    Add AuthRouter to index.js

--- filrouge/routes/index.js
+++ filrouge/routes/index.js
@@ -1,9 +1,11 @@
 import { Router, json } from "express";
 import postsRouter from "./posts";
-import { authentificate} from "../middlewares";
+import AuthRouter from "./Auth";
+import {authentificate} from "../middlewares";
 
 const router = Router();
 router.use(json());
 router.use("/posts",authentificate, postsRouter);
+router.use("/Auth", AuthRouter);
 
 export default router;
