84667f135cf760b23dd7233b6bf5f2ddbf0bf185
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-03-28 14:39:50 +0000

    Add savePost and saveDposts routes to userRouter

--- filrouge/routes/Auth.js
+++ filrouge/routes/Auth.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
+import { authentificate } from "../middlewares";
 const AuthRouter = Router();
 AuthRouter.post("/login", UserController.login);
 AuthRouter.post("/register", UserController.register);
+AuthRouter.post("/savePost/:id",authentificate, UserController.savePost);
+AuthRouter.get("/savedPosts",authentificate, UserController.saveDposts);
 export default AuthRouter;
--- filrouge/services/users.js
+++ filrouge/services/users.js
@@ -13,6 +13,18 @@ export class UserService {
 			throw err;
 		}
 	}
+	static async saveDposts(userr) {
+		try {
+			let user = await User.findById(userr._id).populate("savedPosts")
+			if (!user) throw new NotFound("user not found");
+			return user;
+		} catch (err) {
+			if (err instanceof Error.CastError) {
+				throw new BadRequest("Invalid id");
+			}
+			throw err;
+		}
+	}
 	static async login(user) {
 		try {
 			const foundUser = await User.findOne({ email: user.email });
@@ -22,22 +34,48 @@ export class UserService {
 				message: "Invalid email or password",
 			};
 
-			
-			if (await verify(foundUser.password, user.password)){
-			if (foundUser.banned) return {
-				err: true,
-				message: "You are banned",
-			};
-			let token = generateJWT(foundUser.toJSON());
-			return {
-				err: false,
-				data: { token, user: foundUser.toJSON() },
-			};}
+
+			if (await verify(foundUser.password, user.password)) {
+				if (foundUser.banned) return {
+					err: true,
+					message: "You are banned",
+				};
+				let token = generateJWT(foundUser.toJSON());
+				return {
+					err: false,
+					data: { token, ...foundUser.toJSON() },
+				};
+			}
 
 			return {
 				err: true,
 				message: "Invalid email or password",
 			};
+		} catch (err) {
+			throw err;
+		}
+	}
+	static async savePost(userId, id) {
+		try {
+			let foundUser = await User.findById(userId);
+			if (foundUser.savedPosts.includes(id)) {
+
+				let user = await User.findOneAndUpdate(
+					{ _id: userId },
+					{ $pull: { savedPosts: id } },
+					{ new: true }
+				);
+				return user;
+			} else {
+				let user = await User.findOneAndUpdate(
+					{ _id: userId },
+					{ $addToSet: { savedPosts: id } },
+					{ new: true }
+				);
+				return user;
+			}
+
+
 		} catch (err) {
 			throw err;
 		}
