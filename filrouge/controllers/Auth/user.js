839160b21092938de412cecd67bf1d947eec9b23
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-03-28 14:37:30 +0000

    Add saveDposts and savePost methods to UserController

--- filrouge/controllers/Auth/user.js
+++ filrouge/controllers/Auth/user.js
@@ -2,10 +2,17 @@
 import { UserService } from "../../services";
 
 export class UserController {
+	static async saveDposts(req, res) {
+		return res.status(200).json(await UserService.saveDposts(req.user));
+	}
 	static async login(req, res) {
 		return res.status(200).json(await UserService.login(req.body));
 	}
 	static async register(req, res) {
 		return res.status(201).json(await UserService.register(req.body));
 	}
+	static async savePost(req, res) {
+		return res.status(200).json(await UserService.savePost(req.user, req.params.id));
+	}
+
 }
