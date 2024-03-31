7135c6adf3efb700009702f527a6e51c5b6c21af
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-03-31 14:51:36 +0000

    Add authentication check in Like component

--- filrougeFront/src/components/Actions/Like.jsx
+++ filrougeFront/src/components/Actions/Like.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import { PiHandsClappingDuotone } from "react-icons/pi";
 import { useSelector ,useDispatch } from "react-redux";
 import { likePost } from "../../Redux/Posts/actions";
+import { Blog } from "../../Context/Context";
 const Like = () => {
- const {user} = useSelector(state => state.users)
+ const {user,logged} = useSelector(state => state.users)
  const {post:{likes ,_id:postId}} = useSelector(state => state.posts)
+ const {setAuthModel}=Blog()
  const dispatch = useDispatch()
   const handleLike =  () => {
+    if(!logged) setAuthModel(true)
   dispatch(likePost(postId))
       
   };
   return (
     <button onClick={handleLike} className="flex items-center gap-1 text-sm">
       <PiHandsClappingDuotone
-        className={`text-xl ${likes?.includes(user._id) ? "text-black" : "text-gray-500"}`}
+        className={`text-xl ${likes?.includes(user?._id) ? "text-black" : "text-gray-500"}`}
       />
       <span>{likes?.length}</span>
     </button>
