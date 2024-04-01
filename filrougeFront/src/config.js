0811fac5eaa714b25f1ec73a782e865776113002
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-04-01 22:00:45 +0000

    Add config.js file with axios configuration

---
+++ filrougeFront/src/config.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000";
+axios.interceptors.request.use((req) => {
+  if (!localStorage.getItem('user')) return req
+  const user  =JSON.parse(localStorage.getItem('user'))
+  const token = user.token
+      req.headers.Authorization =`Bearer ${token}`;
+    
+
+    return req;
+  });
+  export default axios;
