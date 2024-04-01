5ffa2eff56eddefbf41f43e179f6f53531700e68
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-04-01 18:55:35 +0000

    Add adminReducer to Store.js

--- filrougeFront/src/Redux/Store.js
+++ filrougeFront/src/Redux/Store.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import {thunk} from 'redux-thunk';
 import { postsReducer } from './Posts/reducer';
 import { usersReducer } from './Users/reducer';
+import { adminReducer } from './Admin/reducer';
 const blogreducers = combineReducers({
     posts: postsReducer,
-    users: usersReducer
+    users: usersReducer,
+    admin :adminReducer
+    
 });
 const store = createStore(blogreducers,applyMiddleware(thunk));
 export default store;
