fcbc3bbc855da745f8063ef5dd0157eb8a2ecf28
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-04-01 22:02:18 +0000

    mainjs with necessairy providers

--- filrougeFront/src/main.jsx
+++ filrougeFront/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Context from './Context/Context'
+import { Provider } from 'react-redux'
+import store from './Redux/Store'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+      <Context>
+        <Provider store={store}>
+        <App />
+        </Provider>
+      </Context>
+    </BrowserRouter>
   </React.StrictMode>,
 )
